9f35c08825f19b59e153dfe8f3f48056e2f9d136
Slashscreen <SlashScreen@users.noreply.github.com>
2023-04-19 20:20:54 -0700

    git is weird. trying to use sparse checkout to avoid cloning so much.

--- sync.rb
+++ sync.rb
@@ -5,23 +5,29 @@ require 'fileutils'
 require 'yaml'
 
 
-# load requirements from file
-reqs = YAML.load_file("requirements.yaml")
-
-reqs.each do |k, v|
-	repo = v["repo"]
-	src_folder = v["src_folder"]
-	dest_folder = v["dest_folder"]
-	# clone repo
-	if Dir.exists? "#{k}"
+def load_requirements_file(path)	
+	# load requirements from file
+	reqs = YAML.load_file(path)
+	
+	reqs.each do |k, v|
+		repo = v["repo"]
+		src_folder = v["src_folder"]
+		dest_folder = v["dest_folder"]
+		# clone repo
+		if Dir.exists? "#{k}"
+			FileUtils.remove_dir "#{k}"
+		end
+		Git.clone(repo)
+		# `git clone --no-checkout #{repo} && cd #{k} && git sparse-checkout init --cone && git sparse-checkout set #{src_folder}`
+		# move folder
+		unless Dir.exists? "#{dest_folder}"
+			FileUtils.mkdir_p "#{dest_folder}"
+		end
+		FileUtils.mv "#{k}/#{src_folder}", "#{dest_folder}/", :force => true
+		# remove repo
 		FileUtils.remove_dir "#{k}"
 	end
-	git = Git.clone repo
-	# move folder
-	unless Dir.exists? "#{dest_folder}"
-		FileUtils.mkdir_p "#{dest_folder}"
-	end
-	FileUtils.mv "#{k}/#{src_folder}", "#{dest_folder}/", :force => true
-	# remove repo
-	FileUtils.remove_dir "#{k}"
 end
+
+
+load_requirements_file "requirements.yaml"
